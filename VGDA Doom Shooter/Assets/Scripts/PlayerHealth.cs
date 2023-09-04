219e74a0c7f40b7e33d023c9b65737d064d1752e
Lizeth-V <Lizeth.valdovinosrodriguez01@student.csulb.edu>
2023-09-03 17:49:56 -0700

    Player Health + Enemy Damage Scripts

---
+++ VGDA Doom Shooter/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int health;
+    public int maxHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
